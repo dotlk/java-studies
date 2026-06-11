c1f9a3f98fc2d2adec0a614194a7919e2352ca7c
dotlk <173015255+dotlk@users.noreply.github.com>
2026-06-10 21:42:57 -0300

    Início de portfólio - aprendendo java

---
+++ Classes.java
@@ -0,0 +1,12 @@
+public class Classes {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+    
+    //java 21+
+    void main() {
+        System.out.println("Olá, mundo!");
+        
+    }
+    
+}
---
+++ Variaveis.java
@@ -0,0 +1,24 @@
+public class Variaveis {
+    
+    public static void main(String[] args) {
+
+        //Tipos de dados numéricos inteiros
+
+        byte idadeByte = 25; 
+        short idadeShort = 25; 
+        int idadeInt = 25; 
+        long idadeLoncg = 25L; 
+
+        //Tipos de dados numéricos de ponto flutuante
+        float salarioFloat = 5000.50f; 
+        double salarioDouble = 5000.50; 
+        
+        //Tipos de dados textuais
+        String nome = "Lucas";
+        char letra = 'A'; 
+ 
+       //Tipos de dados booleanos
+        boolean isEstudante = true; 
+
+    }
+}
